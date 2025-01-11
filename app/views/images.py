50a2d841b41874abfdfb986c93233660b23fe624
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-12 02:25:44 +0900

    📝 Update code
    read 쿼리구조 개선 id를 가져와 해당 값을 가져오는 쿼리를 두번 실행하는 구조에서 해당 조건을 한번에 조회하여 쿼리문 한번만 날리는 구조로 개선

--- app/views/images.py
+++ app/views/images.py
@@ -1,19 +1,27 @@
-from models import Image
+from models import Image,Question
 from config import db
-
-
-def get_image(questions):
-    images = Image.query.filter_by(Image.questions.contains(questions)).all()
-    if images:
-        return [image.to_dict() for image in images]
-    else: 
+# 모든 이미지들을 가져오기(관리자)
+def get_all_images():
+    images = Image.query.all()
+    if not images:
+        return {"msg": "No Data Found"}
+    return [image.to_dict() for image in images]
+    
+# 해당 question_title의 모든 image 조회
+def get_image(question_title):
+    images = db.session.query(Image).join(Question).filter(Question.title == question_title).all()
+    if not images:
         return {"msg":"No Data Found"}
-def post_image(questions, url, type):
-    new_image = Image(questions=questions, url=url,type=type)
-    if new_image:
-        db.session.add(new_image)
-        db.session.commit()
-        return new_image.to_dict()
-    else:
-        return {"msg":"No Data Found"}
+    return [image.to_dict() for image in images]
+
+def post_image(question_title, url, type):
+    question = Question.query.filter_by(title=question_title).first()
+    
+    if not question:
+        return {"msg": "No Question Found with the given title"}
+    new_image = Image(questions=question, url=url,type=type)
+    
+    db.session.add(new_image)
+    db.session.commit()
+    return new_image.to_dict()
     
--- app/views/question.py
+++ app/views/question.py
@@ -1,5 +1,11 @@
 from models import Question
 from config import db
+# 모든 질문을 가져오기 (관리자)
+def get_all_question():
+    questions = Question.query.all()
+
+    return [question.to_dict() for question in questions]
+
 # 해당 질문 제목의 내용을 가져오는 함수
 def get_question(question_title):
     questions = Question.query.filter_by(title=question_title).all()
@@ -7,7 +13,8 @@ def get_question(question_title):
         return{"msg":"No Data Found"}
     else:
         return [question.to_dict() for question in questions]
-    
+
+# 질문 등록
 def post_question(question_title, is_active,sqe,image_id):
     new_question = Question(
             title=question_title,

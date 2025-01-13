0fdc89994e0e0e301da28eaeec277ff12c8aaff9
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-14 02:39:54 +0900

    ✨ Add CRUD functionality to users API
    추가, 삭제, 수정 및 전반적인 쿼리문 수정

--- app/views/users.py
+++ app/views/users.py
@@ -1,23 +1,45 @@
-from flask import jsonify
+from flask import request
 from app.models import User 
 from config import db
 
-#생성함수
-def create_user(username, email, age, gender):
-    new_user = User(name=username, email=email, age=age, gender=gender)
-    db.session.add(new_user)
-    db.session.commit()
-    
-    return new_user.to_dict()
-
-
-#전체 유저 데이터 조회 함수
-
-def get_users():
+#관리자
+#전체 유저 데이터 조회 함수(관리자)
+def get_all_users():
     users = User.query.all()
     return [user.to_dict() for user in users]
 
-
+def update_user(user_id, username, age, gender, email):
+    user = User.query.get(user_id)
+    if user:
+        if username:
+            user.name = username
+        if age:
+            user.age = age
+        if gender:
+            user.gender = gender
+        if email:
+            user.email = email
+        db.session.commit()
+        return user.to_dict()
+    return {"msg:Not Found User Data"}
+# 유저 삭제
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        return {"msg": "Successfully delete data"}
+    return {"msg":"Not Found Data"}
+    
+# 유저
+#생성함수
+def create_user(username, email, age, gender):
+    if username and email and age and gender:
+        new_user = User(name=username, email=email, age=age, gender=gender)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return new_user.to_dict()
+    return {"msg":"Missing required fields"}
 
 #특정유저조회함수   
 def get_user(user_id):

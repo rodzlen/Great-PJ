ff645296093e617e29d6cc3ec11a83fa2294603b
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-14 02:38:19 +0900

    ✨ Add CRUD functionality to images API
    수정, 게시, 삭제 methods 함수 추가

--- app/views/images.py
+++ app/views/images.py
@@ -6,18 +6,33 @@ def get_all_images():
     if not images:
         return {"msg": "No Data Found"}
     return [image.to_dict() for image in images]
-    
-# 해당 question_title의 모든 image 조회
-def get_image(question_title):
-    images = db.session.query(Image).join(Question).filter(Question.title == question_title).all()
-    if not images:
-        return {"msg":"No Data Found"}
-    return [image.to_dict() for image in images]
-
+# 햐당 이미지 조회
+def get_image(image_id):
+    image = Image.query.filter_by(id=image_id).first()
+    if image:
+        return image.to_dict()
+    return {"msg":"Not Found Image Data"}
+# 이미지 등록
 def post_image( url, type):
     new_image = Image(url=url,type=type)
     
     db.session.add(new_image)
     db.session.commit()
     return new_image.to_dict()
+# 이미지 수정
+def update_image(image_id,url,type):
+    image = Image.query.filter_by(id=image_id).first()
+    image.url=url
+    image.type=type
+    db.session.commit()
+    return image.to_dict()
+
+# 이미지 삭제 
+def delete_image(image_id):
+    image = Image.query.filter_by(id = image_id).first()
+    if image:
+        db.session.delete(image)
+        db.session.commit()
+        return {"msg":"Successfully Deleted Image"}
+    return {"msg":"Not Found Image Data"}
     

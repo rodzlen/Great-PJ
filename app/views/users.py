1542f9c2a2e03c14e2eb5db26f04ee0d2c709c12
rodzlen <qkqhrkxdkdy@naver.com>
2025-01-15 05:54:47 +0900

    🐛 Fix response massage
    유저 생성시 반환 메세지 수정

--- app/views/users.py
+++ app/views/users.py
@@ -33,13 +33,23 @@ def delete_user(user_id):
 # 유저
 #생성함수
 def create_user(username, email, age, gender):
-    if username and email and age and gender:
-        new_user = User(name=username, email=email, age=age, gender=gender)
-        db.session.add(new_user)
-        db.session.commit()
-        
-        return new_user.to_dict()
-    return {"msg":"Missing required fields"}
+    if not all([username, email, age, gender]):
+        return {"message": "모든 필드를 입력해주세요."}
+
+    # 이름 중복 확인
+    existing_user = User.query.filter_by(name=username).first()
+    if existing_user:
+        return {"message": "이미 존재하는 계정입니다."}
+
+    # 새로운 유저 생성
+    new_user = User(name=username, email=email, age=age, gender=gender)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return {
+        "message": f"{new_user.name}님 회원가입을 축하합니다.",
+        "user_id": new_user.id,
+    }
 
 #특정유저조회함수   
 def get_user(user_id):
